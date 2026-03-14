0ea16af9e17ae6dd628cd2ce27eaac7783d814f3
Vix-Laptop <vsthakur451@gmail.com>
2026-03-14 16:56:01 +0100

    Move the note movement functionality from note.jsx to index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,53 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-
 const root = ReactDOM.createRoot(rootElement);
 
+let isSelected = false;
+console.log(isSelected);
+
+const mousePos = { "x": "", "y": "" };
+
 document.addEventListener("mousemove", logMousePos);
-
-const mousePos = {"x": "", "y": ""};
-
 function logMousePos(event) {
     mousePos.x = event.screenX;
     mousePos.y = event.screenY;
 }
 
+document.addEventListener("click", handleClick);
+function handleClick(event) {
+    if (!isSelected) {        
+        handlePickUp(event);
+    }
+
+    else {
+        handleMove();
+    }
+}
+
+let targetNote = null;
+
+function handlePickUp(event) {    
+    targetNote = event.target;
+    if (targetNote.className !== "note") return;
+
+    console.log("Note Selected");
+    isSelected = true;    
+}
+
+function handleMove() {
+    let style = targetNote.style;
+
+    style.position = "absolute";
+    style.left = mousePos.x+"px";
+    style.top = mousePos.y+"px";
+
+    isSelected = false;
+    targetNote = null;
+}
+
 root.render(
     <React.StrictMode>
-        <App mousePos = {mousePos}/>
+        <App mousePos={mousePos} />
     </React.StrictMode>
 );
